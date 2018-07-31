25783b7e555c05c7537e4b285e422c2081bfa0f3
Chen Yu Pao <windperson@gmail.com>
2018-07-31 20:34:50 +0800

    Convert to FP implement

--- ReativeAsyncAwaitDemo/ReativeAsyncAwaitDemo/MainPage.xaml.cs
+++ ReativeAsyncAwaitDemo/ReativeAsyncAwaitDemo/MainPage.xaml.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Reactive.Linq;
+using System.Reactive.Threading.Tasks;
 using System.Text;
 using System.Threading.Tasks;
 using dotMorten.Xamarin.Forms;
@@ -11,8 +14,8 @@ namespace ReativeAsyncAwaitDemo
 {
     public partial class MainPage : ContentPage
     {
-        private Search _search;
-
+        private readonly Search _search;
+        private readonly IDisposable _subscribe;
 
         public MainPage()
         {
@@ -24,42 +27,80 @@ namespace ReativeAsyncAwaitDemo
                 IsSecure = true,
                 SkipDisambiguation = false,
             };
+            _subscribe = SetupUIeventStream();
         }
 
-        private async void AutoSuggestBox_TextChanged(object sender, AutoSuggestBoxTextChangedEventArgs e)
+        ~MainPage()
         {
-            var searchBox = (AutoSuggestBox) sender;
-
-            if (e.Reason != AutoSuggestionBoxTextChangeReason.UserInput)
-            {
-                return;
-            }
-            var searchText = searchBox.Text;
-            if (string.IsNullOrWhiteSpace(searchText) || searchText.Length < 3)
-            {
-                searchBox.ItemsSource = null;
-            }
-            else
-            {
-                var suggestions = await GetSuggestions(searchText);
-                searchBox.ItemsSource = suggestions.ToList();
-            }
+            _subscribe?.Dispose();
         }
 
-        private async Task<IEnumerable<QueryResult>> GetSuggestions(string searchText)
+        private IDisposable SetupUIeventStream()
         {
-            var searchResult = await _search.QueryAsync(searchText, Guid.NewGuid().ToString());
+            return Observable.FromEventPattern<EventHandler<AutoSuggestBoxTextChangedEventArgs>, AutoSuggestBoxTextChangedEventArgs>(
+                      ev => SearchTextBox.TextChanged += ev,
+                      ev => SearchTextBox.TextChanged -= ev)
+                  .Throttle(TimeSpan.FromMilliseconds(500))
+                  .Where(x => x.EventArgs.Reason == AutoSuggestionBoxTextChangeReason.UserInput)
+                  .Select(x => ((AutoSuggestBox)x.Sender).Text)
+                  .Where(txt => !string.IsNullOrWhiteSpace(txt) && txt.Length >= 3)
+                  .DistinctUntilChanged()
+                  .SelectMany(queryStr => GetSuggestions(queryStr, _search).ToObservable())
+                  .Subscribe(value =>
+                  {
+                      Device.BeginInvokeOnMainThread(() =>
+                    {
+                         Debug.WriteLine("update suggestion list");
+                         SearchTextBox.ItemsSource = value.ToList();
+                     });
+                  });
+        }
+
+        private static async Task<IEnumerable<QueryResult>> GetSuggestions(string searchText, Search search)
+        {
+            Debug.WriteLine("invoke Web API");
+            var searchResult = await search.QueryAsync(searchText, Guid.NewGuid().ToString());
             return searchResult.RelatedTopics.Where(x => !string.IsNullOrEmpty(x.Text));
         }
 
-        private void AutoSuggestBox_QuerySubmitted(object sender, AutoSuggestBoxQuerySubmittedEventArgs e)
-        {
-            //not implement
-        }
 
-        private void AutoSuggestBox_SuggestionChosen(object sender, AutoSuggestBoxSuggestionChosenEventArgs e)
-        {
-            // not implment
-        }
+        //private async void AutoSuggestBox_TextChanged(object sender, AutoSuggestBoxTextChangedEventArgs e)
+        //{
+        //    var searchBox = (AutoSuggestBox) sender;
+
+        //    if (e.Reason != AutoSuggestionBoxTextChangeReason.UserInput)
+        //    {
+        //        return;
+        //    }
+        //    var searchText = searchBox.Text;
+        //    if (string.IsNullOrWhiteSpace(searchText) || searchText.Length < 3)
+        //    {
+        //        searchBox.ItemsSource = null;
+        //    }
+        //    else
+        //    {
+        //        var suggestions = await GetSuggestions(searchText);
+        //        searchBox.ItemsSource = suggestions.ToList();
+        //    }
+        //}
+
+        //private async Task<IEnumerable<QueryResult>> GetSuggestions(string searchText)
+        //{
+        //    var searchResult = await _search.QueryAsync(searchText, Guid.NewGuid().ToString());
+        //    return searchResult.RelatedTopics.Where(x => !string.IsNullOrEmpty(x.Text));
+        //}
+
+
+
+
+        //private void AutoSuggestBox_QuerySubmitted(object sender, AutoSuggestBoxQuerySubmittedEventArgs e)
+        //{
+        //    //not implement
+        //}
+
+        //private void AutoSuggestBox_SuggestionChosen(object sender, AutoSuggestBoxSuggestionChosenEventArgs e)
+        //{
+        //    // not implment
+        //}
     }
 }
