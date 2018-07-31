fdc84b37fee23e0cdda64bce4bb97f1f6f8f3f75
Chen Yu Pao <windperson@gmail.com>
2018-07-31 12:53:10 +0800

    init project structure

--- ReativeAsyncAwaitDemo/ReativeAsyncAwaitDemo.Android/MainActivity.cs
+++ ReativeAsyncAwaitDemo/ReativeAsyncAwaitDemo.Android/MainActivity.cs
@@ -9,7 +9,9 @@ using Android.OS;
 
 namespace ReativeAsyncAwaitDemo.Droid
 {
-    [Activity(Label = "ReativeAsyncAwaitDemo", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "ReativeAsyncAwaitDemo", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, 
+        ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation,
+        ScreenOrientation = ScreenOrientation.Portrait)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
